a69d43a9bd599dbfc85398411207321208550b8d
edara yogesh <yogeshedara305@gmail.com>
2026-08-12 09:15:57 +0530

    make responsive for the mobile for 375px

---
+++ script.js
@@ -0,0 +1,7 @@
+ var menu=document.querySelector(".mobile-nav-btn")
+console.log(menu);
+ 
+ function menuopen(){
+     var mobilenav=document.querySelector(".mobile-nav");
+     mobilenav.style.display="block"
+ }
