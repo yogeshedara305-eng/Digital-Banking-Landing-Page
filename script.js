ccb02408e749980977ddf55a11861979d895ce39
edara yogesh <yogeshedara305@gmail.com>
2026-08-12 09:22:20 +0530

    removed menu bar for the mobile

--- script.js
+++
@@ -1,7 +0,0 @@
- var menu=document.querySelector(".mobile-nav-btn")
-console.log(menu);
- 
- function menuopen(){
-     var mobilenav=document.querySelector(".mobile-nav");
-     mobilenav.style.display="block"
- }
